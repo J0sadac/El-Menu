6c4df3fefb99e3ef146d25518ec4d5260dcc71fb
Josadac Castañeda <jugdmentc12@gmail.com>
2022-05-17 14:27:21 -0500

    arregle el problema con las imagenes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import Logo from '../image/logos/app-icon.png'
 
 const NavBar = () => {
 
     return(
             <nav className="navbar">
                 <div className="menu-principal">
-                    <Link to='/' className="lista">Home</Link>
+                    <Link to='/' className="lista"><img src={Logo} alt='El Menu' className="img-navbar"/></Link>
                 </div>
 
                 <div className="opciones">
